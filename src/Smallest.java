e160be4fc7fbfc3e8501d9d953c247ed7d75628b
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-02-12 00:20:34 +0530

    final commit

--- src/Smallest.java
+++ src/Smallest.java
@@ -1,3 +1,21 @@
-public class Smallest {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Smallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            sc.nextLine();
+            String s = sc.nextLine();
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String str = new String(ch);
+            if (str.compareTo(s) < 0) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
 }
